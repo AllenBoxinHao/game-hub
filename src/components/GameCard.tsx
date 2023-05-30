f03283b5c992e2cb386f5b21a00577b3092717fd
AllenBoxinHao <allen7nella@gmail.com>
2023-05-30 22:37:55 +1000

    cleanup the game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,12 +12,12 @@ const GameCard = ({ game }: Props) => {
     return (
         <Card>
             <Image src={getCroppedImageUrl(game.background_image)} />
-            <CardBody minHeight="120px">
-                <Heading fontSize={"2xl"}>{game.name}</Heading>
-                <HStack justifyContent="space-between">
+            <CardBody minHeight="140px">
+                <HStack justifyContent="space-between" marginBottom={3}>
                     <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
                     <CriticScore score={game.metacritic} />
                 </HStack>
+                <Heading fontSize={"2xl"}>{game.name}</Heading>
             </CardBody>
         </Card>
     );
