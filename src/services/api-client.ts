8b8f05851867447b0614eebf66262f89b463ae51
AllenBoxinHao <allen7nella@gmail.com>
2023-08-04 17:43:10 +1000

    Refactor: reduce APIClient duplication

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,27 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
     count: number;
     results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
         key: "0a00d7e22e6d482896d15a2b66499bba",
     },
 });
+
+class APIClient<T> {
+    endpoint: string;
+
+    constructor(endpoint: string) {
+        this.endpoint = endpoint;
+    }
+
+    getAll = (config: AxiosRequestConfig) => {
+        return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then((res) => res.data);
+    };
+}
+
+export default APIClient;
